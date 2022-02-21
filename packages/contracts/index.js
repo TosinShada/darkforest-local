6d54d5abe7bc77d389bc1c7bfe186e5a53e046e4
Tosin Shada <shardoeski@gmail.com>
2022-02-21 21:05:21 +0100

    commit

---
+++ packages/contracts/index.js
@@ -0,0 +1,110 @@
+'use strict';
+/**
+ * This package contains deployed contract addresses, ABIs, and Typechain types
+ * for the Dark Forest game.
+ *
+ * ## Installation
+ *
+ * You can install this package using [`npm`](https://www.npmjs.com) or
+ * [`yarn`](https://classic.yarnpkg.com/lang/en/) by running:
+ *
+ * ```bash
+ * npm install --save @darkforest_eth/contracts
+ * ```
+ * ```bash
+ * yarn add @darkforest_eth/contracts
+ * ```
+ *
+ * When using this in a plugin, you might want to load it with [skypack](https://www.skypack.dev)
+ *
+ * ```js
+ * import * as contracts from 'http://cdn.skypack.dev/@darkforest_eth/contracts'
+ * ```
+ *
+ * ## Typechain
+ *
+ * The Typechain types can be found in the `typechain` directory.
+ *
+ * ## ABIs
+ *
+ * The contract ABIs can be found in the `abis` directory.
+ *
+ * @packageDocumentation
+ */
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.SCORING_CONTRACT_ADDRESS =
+  exports.GPT_CREDIT_CONTRACT_ADDRESS =
+  exports.WHITELIST_CONTRACT_ADDRESS =
+  exports.GETTERS_CONTRACT_ADDRESS =
+  exports.TOKENS_CONTRACT_ADDRESS =
+  exports.CORE_CONTRACT_ADDRESS =
+  exports.LAZY_UPDATE_LIBRARY_ADDRESS =
+  exports.INITIALIZE_LIBRARY_ADDRESS =
+  exports.VERIFIER_LIBRARY_ADDRESS =
+  exports.ARTIFACT_UTILS_LIBRARY_ADDRESS =
+  exports.PLANET_LIBRARY_ADDRESS =
+  exports.UTILS_LIBRARY_ADDRESS =
+  exports.START_BLOCK =
+  exports.NETWORK_ID =
+  exports.NETWORK =
+    void 0;
+/**
+ * The name of the network where these contracts are deployed.
+ */
+exports.NETWORK = 'xdai';
+/**
+ * The id of the network where these contracts are deployed.
+ */
+exports.NETWORK_ID = 100;
+/**
+ * The block in which the DarkForestCore contract was deployed.
+ */
+exports.START_BLOCK = 18361165;
+/**
+ * The address for the DarkForestUtils library.
+ */
+exports.UTILS_LIBRARY_ADDRESS = '0x7333aAd0767F8c6A94Fc40Cb9137C6cB9c141a30';
+/**
+ * The address for the DarkForestPlanet library.
+ */
+exports.PLANET_LIBRARY_ADDRESS = '0x46C4c388Ef70798af0409B808b26e72B116e0dBf';
+/**
+ * The address for the DarkForestArtifactUtils library.
+ */
+exports.ARTIFACT_UTILS_LIBRARY_ADDRESS = '0xAb1E73A97548224097bbe32656167d1F720484Ae';
+/**
+ * The address for the Verifier library.
+ */
+exports.VERIFIER_LIBRARY_ADDRESS = '0xD823a200E7b6dFcD11569ff19d7E738cA4ecAf41';
+/**
+ * The address for the DarkForestInitialize library.
+ */
+exports.INITIALIZE_LIBRARY_ADDRESS = '0x4b813c4869E2705d8DfE0c9D123303d8ae9554B1';
+/**
+ * The address for the DarkForestLazyUpdate library.
+ */
+exports.LAZY_UPDATE_LIBRARY_ADDRESS = '0x0304a6b7e6Ffd1132BDA89f5471F22eFe2748045';
+/**
+ * The address for the DarkForestCore contract.
+ */
+exports.CORE_CONTRACT_ADDRESS = '0x27a166aE00C33Bef64306760aCd7C9fD3c2fEB74';
+/**
+ * The address for the DarkForestTokens contract.
+ */
+exports.TOKENS_CONTRACT_ADDRESS = '0x8e7Fc9c67Cf2bc5D001682d17355dc5c7f41e4C1';
+/**
+ * The address for the DarkForestGetters contract.
+ */
+exports.GETTERS_CONTRACT_ADDRESS = '0x71eF8b8D795AEbaf6dd6a2A0397B5D3A0e2B226E';
+/**
+ * The address for the Whitelist contract.
+ */
+exports.WHITELIST_CONTRACT_ADDRESS = '0xed6b8BC35405bF6aa231CDDe9ec994b9DBF1f501';
+/**
+ * The address for the DarkForestGPTCredit contract.
+ */
+exports.GPT_CREDIT_CONTRACT_ADDRESS = '0x204104155100d6bc0d8928d720b3CD8d845B0F28';
+/**
+ * The address for the DarkForestScoring contract.
+ */
+exports.SCORING_CONTRACT_ADDRESS = '';
